9ab8caf752af960844a48c939c6d5898cc8c3e91
Krushna Jadhav <jadhavkrushna449@gmail.com>
2024-01-14 03:44:15 -0800

    first commit

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-employee',
+    template: `<h1>Hello world! </h1>`
+})
+
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
